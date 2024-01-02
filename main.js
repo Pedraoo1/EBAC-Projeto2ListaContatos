e6fdb638137f32246c672d1f9563350c24201534
Pedraoo1 <149252929+Pedraoo1@users.noreply.github.com>
2024-01-02 17:50:15 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+let linhas = "";
+let nomes = [];
+
+
+function adicionar() {
+    let nome = document.querySelector(".nome").value;
+    let DDD = document.querySelector(".DDD").value;
+    let tel = document.querySelector(".tel").value;
+
+    let nomeMaiusculo = nome.toUpperCase();
+
+    if (nome === "" || DDD === "" || tel === "") {
+        alert("Você precisa preencher os campos!");
+    } else if (nomes.includes(nomeMaiusculo)) {
+        alert("Você Já adicionou um outro contato com este nome!");
+    } else {
+        let novaLinha = `<tr>
+            <th>${nome}</th>
+            <th>(${DDD}) ${tel}</th>  
+        </tr>`;
+
+        linhas += novaLinha;
+        document.querySelector(".tbody").innerHTML = linhas;
+
+        let tabela = document.getElementById("table");
+        tabela.classList.remove("naoMostra");
+
+        nomes.push(nomeMaiusculo);
+    }
+}
